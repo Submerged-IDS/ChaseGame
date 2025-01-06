5e1b0c7b62d22af944b2ad4e2ba289fc86dd8364
Submerged-IDS <submergedids@gmail.com>
2025-01-06 07:53:32 +0000

    Add DFS maze

--- MazeGenerator.py
+++ MazeGenerator.py
@@ -32,12 +32,56 @@ class MazeGenerator:
     
     def displayMaze(self):
         return '\n'.join([''.join(map(str, row)) for row in self.grid])
+    
+    def inGrid(self, row, col):
+        return 0 <= row < self.rows and 0 <= col < self.columns
 
+    def checkSquare(self, row, col):
+        return self.inGrid(row, col) and self.grid[row][col] != 1 and self.grid[row][col] != 2
+
+    def deadEnd(self, row, col):
+        dead_end = True
+        if self.checkSquare(row+2, col):
+            dead_end = False
+        if self.checkSquare(row-2, col):
+            dead_end = False
+        if self.checkSquare(row, col+2):
+            dead_end = False
+        if self.checkSquare(row, col-2):
+            dead_end = False
+        return dead_end
+    
+    def checkNeighbours(self, row, col):
+        options = []
+        for option in [[row+2, col], [row-2, col], [row, col+2], [row, col-2]]:
+            if self.checkSquare(option[0], option[1]):
+                options.append(option)
+        chosen = random.choice(options)
+        return chosen[0], chosen[1]
+
+    def generateMaze(self):
+        self.generateBaseGrid()
+        current = [1, 1]
+        self.grid[current[0]][current[1]] = 2
+        movements = [current]
+
+        while movements:
+            if self.deadEnd(current[0], current[1]):
+                movements.pop()
+                if movements:
+                    current = movements[-1]
+                continue
         
+            next_row, next_col = self.checkNeighbours(current[0], current[1])
+            movements.append([next_row, next_col])
+            self.grid[next_row][next_col] = 2
+            self.grid[(current[0]+next_row)//2][(current[1]+next_col)//2] = 2
+            current = [next_row, next_col]
+        return self.grid
 
 if __name__ == "__main__":
     mazeGen = MazeGenerator()
-    maze = mazeGen.generateBaseGrid()
+    maze = mazeGen.generateMaze()
     print(mazeGen.displayMaze())
 
 
